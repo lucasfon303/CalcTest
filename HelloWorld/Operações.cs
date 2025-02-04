9b9de2c744613c64954a4968ac884aa7e1c91ea8
luqui <luqui@LUPCAS>
2025-02-04 15:28:25 -0300

    Adicionar arquivos de projeto.

---
+++ HelloWorld/Operações.cs
@@ -0,0 +1,243 @@
+﻿using System;
+
+namespace Calculadora
+{
+    internal class Operações
+    {
+        public static string erro = "Opção inválida. Pressione ENTER para retornar ao menu inicial.";
+        public static string retornar = "Pressione ENTER para retornar ao menu inicial.";
+        public static bool escolheuSair = false;
+
+        enum SairOp { Sim = 1, Não }
+
+        public void Soma()
+        {
+            Console.Clear();
+            Console.WriteLine("-SOMA-");
+            Console.WriteLine("Informe a primeira parcela:");
+            string a = Console.ReadLine();
+            bool aB = float.TryParse(a, out float aF);
+            if (aB == false)
+            {
+                Console.WriteLine(erro);
+                Console.ReadLine();
+                Console.Clear();
+            }
+            else
+            {
+                Console.WriteLine("Informe a segunda parcela:");
+                string b = Console.ReadLine();
+                bool bB = float.TryParse(b, out float bF);
+                if (bB == false)
+                {
+                    Console.WriteLine(erro);
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                else
+                {
+                    float resultado = aF + bF;
+                    Console.WriteLine($"O total é: {resultado}!");
+                    Console.WriteLine(retornar);
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+
+            }
+        }
+        public void Sub()
+        {
+            Console.Clear();
+            Console.WriteLine("-SUBTRAÇÃO-");
+            Console.WriteLine("Informe o minuendo:");
+            string a = Console.ReadLine();
+            bool aB = float.TryParse(a, out float aF);
+            if (aB == false)
+            {
+                Console.WriteLine(erro);
+                Console.ReadLine();
+                Console.Clear();
+            }
+            else
+            {
+                Console.WriteLine("Informe o subtraendo:");
+                string b = Console.ReadLine();
+                bool bB = float.TryParse(b, out float bF);
+                if (bB == false)
+                {
+                    Console.WriteLine(erro);
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                else
+                {
+                    float resultado = aF - bF;
+                    Console.WriteLine($"A diferença é: {resultado}!");
+                    Console.WriteLine(retornar);
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+
+            }
+        }
+        public void Div()
+        {
+            {
+                Console.Clear();
+                Console.WriteLine("-DIVISÃO-");
+                Console.WriteLine("Informe o dividendo:");
+                string a = Console.ReadLine();
+                bool aB = float.TryParse(a, out float aF);
+                if (aB == false)
+                {
+                    Console.WriteLine(erro);
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.WriteLine("Informe o divisor:");
+                    string b = Console.ReadLine();
+                    bool bB = float.TryParse(b, out float bF);
+                    if (bB == false)
+                    {
+                        Console.WriteLine(erro);
+                        Console.ReadLine();
+                        Console.Clear();
+                    }
+                    else
+                    {
+                        float resultado = aF / bF;
+                        Console.WriteLine($"O quociente é: {resultado}!");
+                        Console.WriteLine(retornar);
+                        Console.ReadLine();
+                        Console.Clear();
+                    }
+
+                }
+            }
+        }
+        public void Mult()
+        {
+            Console.Clear();
+            Console.WriteLine("-MULTIPLICAÇÃO-");
+            Console.WriteLine("Informe o multiplicando:");
+            string a = Console.ReadLine();
+            bool aB = float.TryParse(a, out float aF);
+            if (aB == false)
+            {
+                Console.WriteLine(erro);
+                Console.ReadLine();
+                Console.Clear();
+            }
+            else
+            {
+                Console.WriteLine("Informe o multiplicador:");
+                string b = Console.ReadLine();
+                bool bB = float.TryParse(b, out float bF);
+                if (bB == false)
+                {
+                    Console.WriteLine(erro);
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                else
+                {
+                    float resultado = aF * bF;
+                    Console.WriteLine($"O produto é: {resultado}!");
+                    Console.WriteLine(retornar);
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+
+            }
+        }
+        public void Pow()
+        {
+            {
+                Console.Clear();
+                Console.WriteLine("-POTÊNCIA-");
+                Console.WriteLine("Informe a base:");
+                string a = Console.ReadLine();
+                bool aB = float.TryParse(a, out float aF);
+                if (aB == false)
+                {
+                    Console.WriteLine(erro);
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.WriteLine("Informe o expoente:");
+                    string b = Console.ReadLine();
+                    bool bB = float.TryParse(b, out float bF);
+                    if (bB == false)
+                    {
+                        Console.WriteLine(erro);
+                        Console.ReadLine();
+                        Console.Clear();
+                    }
+                    else
+                    {
+                        float resultado = (float)Math.Pow(aF, bF);
+                        Console.WriteLine($"O total é: {resultado}!");
+                        Console.WriteLine(retornar);
+                        Console.ReadLine();
+                        Console.Clear();
+                    }
+                }
+            }
+        }
+        public void Raiz()
+        {
+            Console.Clear();
+            Console.WriteLine("-RAÍZ QUADRADA-");
+            Console.WriteLine("Informe o radicando:");
+            string a = Console.ReadLine();
+            bool aB = float.TryParse(a, out float aF);
+            if (aB == false)
+            {
+                Console.WriteLine(erro);
+                Console.ReadLine();
+                Console.Clear();
+            }
+            else
+            {
+                float resultado = (float)Math.Sqrt(aF);
+                Console.WriteLine($"O total é: {resultado}!");
+                Console.WriteLine(retornar);
+                Console.ReadLine();
+                Console.Clear();
+            }
+
+        }
+        public void Sair()
+        {
+            Console.Clear();
+            Console.WriteLine("-SAIR-");
+            Console.WriteLine("Você tem certeza que deseja sair?\n1 - Sim\n2 - Não");
+            string opStr2 = Console.ReadLine();
+            bool opBool2 = int.TryParse(opStr2, out int opInt2);
+            if (opBool2 == false)
+            {
+                Console.WriteLine(erro);
+                Console.ReadLine();
+            }
+            else
+            {
+                SairOp opção2 = (SairOp)opInt2;
+                if (opção2 == SairOp.Não)
+                {
+                    Console.WriteLine(retornar);
+                    Console.ReadLine();
+                }
+                else
+                {
+                    escolheuSair = true;
+                }
+            }
+            Console.Clear();
+        }
+    }
+}
+
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        enum Menu { Soma = 1, Subtração, Divisão, Multiplicação, Potência, Raíz, Sair }
+        static void Main(string[] args)
+        {
+            while (Operações.escolheuSair == false)
+            {
+                Console.WriteLine("-CALCULADORA-\nSelecione uma opção para começar:");
+                Console.WriteLine("1 - Soma\n2 - Subtração\n3 - Divisão\n4 - Multiplicação\n5 - Potência\n6 - Raíz Quadrada\n7 - Sair");
+
+                string opStr = Console.ReadLine();
+                Console.Clear();
+                bool opBool = int.TryParse(opStr, out int opInt);
+
+                if (opBool == true)
+                {
+                    Menu opção = (Menu)opInt;
+                    Operações conta = new Operações();
+                    switch (opção)
+                    {
+                        case Menu.Soma:
+                            conta.Soma();
+                            break;
+                        case Menu.Subtração:
+                            conta.Sub();
+                            break;
+                        case Menu.Divisão:
+                            conta.Div();
+                            break;
+                        case Menu.Multiplicação:
+                            conta.Mult();
+                            break;
+                        case Menu.Potência:
+                            conta.Pow();
+                            break;
+                        case Menu.Raíz:
+                            conta.Raiz();
+                            break;
+                        case Menu.Sair:
+                            conta.Sair();
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(Operações.erro);
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
